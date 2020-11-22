fb31cf78ab1210442ca4687d9f2c3d19c2eb75ac
kagafon <kagafon@users.noreply.github.com>
2020-11-22 18:06:59 +0300

    fix: change API path (service re-deploy)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://77bl1dne2l.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ftzn38j7f8.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://lbzixea0ac.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
