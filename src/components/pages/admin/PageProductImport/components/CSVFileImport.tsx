4931276430e7a2cf5c019f7d91104d5652c725bd
kagafon <kagafon@users.noreply.github.com>
2020-11-28 19:51:59 +0300

    fix: add support of authorization_token

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,6 +31,11 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   };
 
   const uploadFile = async (e: any) => {
+    const authorization_token = window.localStorage.getItem(
+      'authorization_token'
+    );
+    console.log('authorization_token: ', authorization_token);
+
     // Get the presigned URL
     const response = await axios({
       method: 'GET',
@@ -38,6 +43,11 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       params: {
         name: encodeURIComponent(file.name),
       },
+      headers: authorization_token
+        ? {
+            Authorization: `Basic: ${authorization_token}`,
+          }
+        : {},
     });
     console.log('File to upload: ', file.name);
     console.log('Uploading to: ', response.data.url);
