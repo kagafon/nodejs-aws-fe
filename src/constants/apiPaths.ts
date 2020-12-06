61c4a129411275b253102caa324e8b3f11970862
kagafon <kagafon@users.noreply.github.com>
2020-12-06 18:17:18 +0300

    fix: update apiPaths to use HTTPS-to-HTP proxy

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://lbzixea0ac.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://kagafon-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://v1sowxxg6e.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
